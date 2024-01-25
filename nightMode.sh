40da1f7e951226478b0fae7e21c9a7ef5c23fc0a
JAcKFiDo <jackfido@users.noreply.github.com>
2024-01-25 17:02:07 -0600

    Update nightMode.sh
    I used this script on the network to get sunrise and sunset values to use this values
    
    https://linuxconfig.org/how-to-obtain-sunrise-sunset-time-for-any-location-from-linux-command-line

--- nightMode.sh
+++ nightMode.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-hour=$(date +%H)
 
-if [  $hour -gt 6 ] && [ $hour -lt 19 ];
+# First obtain a location code from: https://weather.codes/search/
+
+# Insert your location. For example LOXX0001 is a location code for Bratislava, Slovakia
+# Mi ciudad mas proxima es San Antonio
+location="USTX1200"
+tmpfile=/tmp/$location.out
+
+# Obtain sunrise and sunset raw data from weather.com
+wget -q "https://weather.com/weather/today/l/$location" -O "$tmpfile"
+
+SUNR=$(grep SunriseSunset "$tmpfile" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | head -1)
+SUNS=$(grep SunriseSunset "$tmpfile" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | tail -1)
+
+sunrise=$(date --date="$SUNR" +%Y%m%d%H%M)
+sunset=$(date --date="$SUNS" +%Y%m%d%H%M)
+
+# Use $sunrise and $sunset variables to fit your needs. Example:
+echo "Sunrise for location $location: $sunrise"
+echo "Sunset for location $location: $sunset"
+
+hour=$(date +%Y%m%d%H%M)
+
+echo "Actual time is: $sunrise"
+
+if [  $hour -gt $sunrise ] && [ $hour -lt $sunset ];
 then
-    echo "Day theme established"
+    echo "Aplicar tema de dia"
     bash ~/Public/nightMode/day.sh
 fi
 
-if [ $hour -lt 7 ] || [ $hour -gt 18 ];
+if [ $hour -lt $sunrise ] || [ $hour -gt $sunset ];
 then
-    echo "Night theme established"
+    echo "Aplicar tema de noche"
     bash ~/Public/nightMode/night.sh
 fi
