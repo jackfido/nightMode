a34bdf72571ac0ef3fde577722bb0c371bea20fe
Juan Delgado <alfdelgadomtz@gmail.com>
2024-02-04 01:33:27 -0600

    Remove ignored

--- getSunRiseAndSet.sh
+++ getSunRiseAndSet.sh
@@ -9,4 +9,11 @@ tmpfile=~/Public/nightMode/tmp/$location.out
 
 # Obtain sunrise and sunset raw data from weather.com
 wget -q "https://weather.com/weather/today/l/$location" -O "$tmpfile"
+echo "San Antonio time getted successfully"
 
+location="monterrey"
+tmpfile=~/Public/nightMode/tmp/$location.out
+
+wget -q "https://www.timeanddate.com/sun/mexico/$location" -O "$tmpfile"
+
+echo "Monterrey time getted successfully"
