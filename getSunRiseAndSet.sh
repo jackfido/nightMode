6805d270d41da562c7ca356a69b9372889c9ffca
Juan Delgado <alfdelgadomtz@gmail.com>
2024-02-04 01:38:56 -0600

    Remove US service
    Add Mexico Sunset/Sunrise web page

--- getSunRiseAndSet.sh
+++ getSunRiseAndSet.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
+# First obtain a location code from: https://www.timeanddate.com/sun/mexico/
 
-# First obtain a location code from: https://weather.codes/search/
-
-# Insert your location. For example USTX1200 is a location code for San Antonio, Texas
-location="USTX1200"
-tmpfile=~/Public/nightMode/tmp/$location.out
-
-# Obtain sunrise and sunset raw data from weather.com
-wget -q "https://weather.com/weather/today/l/$location" -O "$tmpfile"
-echo "San Antonio time getted successfully"
+# Insert your location. For example 'monterrey' is a location for Nuevo León, México
 
 location="monterrey"
 tmpfile=~/Public/nightMode/tmp/$location.out
--- nightMode.sh
+++ nightMode.sh
@@ -29,31 +29,3 @@ then
     bash ~/Public/nightMode/night.sh
 fi
 
-location2="USTX1200"
-tmpfile2=~/Public/nightMode/tmp/$location2.out
-
-SUNR2=$(grep SunriseSunset "$tmpfile2" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | head -1)
-SUNS2=$(grep SunriseSunset "$tmpfile2" | grep -oE '((1[0-2]|0?[1-9]):([0-5][0-9]) ?([AaPp][Mm]))' | tail -1)
-
-sunrise2=$(date --date="$SUNR2" +%Y%m%d%H%M)
-sunset2=$(date --date="$SUNS2" +%Y%m%d%H%M)
-
-# Use $sunrise and $sunset variables to fit your needs. Example:
-echo "Sunrise for location $location2: $sunrise2"
-echo "Sunset for location $location2: $sunset2"
-
-hour2=$(date +%Y%m%d%H%M)
-
-echo "Actual time 2 is: $(date +%Y%m%d%H%M)"
-
-if [  $hour2 -gt $sunrise2 ] && [ $hour2 -lt $sunset2 ];
-then
-    echo "Aplicar tema de dia"
-    bash ~/Public/nightMode/day.sh
-fi
-
-if [ $hour2 -lt $sunrise2 ] || [ $hour2 -gt $sunset2 ];
-then
-    echo "Aplicar tema de noche"
-    bash ~/Public/nightMode/night.sh
-fi
