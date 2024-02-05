5b8afaab24f703bea406b172bbe3d6f0c91f5e3f
Juan Delgado <alfdelgadomtz@gmail.com>
2024-02-05 14:04:04 -0600

    fix Variable theme2Stablish
    $theme2Stablish not theme2Stablish

--- day.sh
+++ day.sh
@@ -6,9 +6,9 @@ export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/en
 theme2Stablish='Materia-light-compact'
 theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
-if [ "$theme" != "theme2Stablish" ];
+if [ "$theme" != "$theme2Stablish" ];
 then
-	gsettings set org.gnome.desktop.interface gtk-theme theme2Stablish
+	gsettings set org.gnome.desktop.interface gtk-theme $theme2Stablish
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 	gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
 #    notify-send "Day theme stablished"
