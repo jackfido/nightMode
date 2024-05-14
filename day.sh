61ff57dad30684e8429dc22f345d0162cd069baf
Juan Delgado <alfdelgadomtz@gmail.com>
2024-05-13 21:52:05 -0600

    Add general theme
    Add yaru as default

--- day.sh
+++ day.sh
@@ -3,13 +3,15 @@
 PID=$(pgrep compiz)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-theme2Stablish='Materia-light-compact'
+# theme2Stablish='Materia-light-compact'
+theme2Stablish='Yaru'
 theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
 if [ "$theme" != "$theme2Stablish" ];
 then
 	gsettings set org.gnome.desktop.interface gtk-theme $theme2Stablish
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
-	gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
+#	gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
+	gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple'
 #    notify-send "Day theme stablished"
 fi
--- night.sh
+++ night.sh
@@ -3,13 +3,15 @@
 PID=$(pgrep compiz)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
 
-theme2Stablish='Materia-dark-compact'
+# theme2Stablish='Materia-dark-compact'
+theme2Stablish='Yaru-unity-dark'
 theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
 if [ "$theme" != "$theme2Stablish" ];
 then
 	gsettings set org.gnome.desktop.interface gtk-theme $theme2Stablish
 	gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-	gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+#	gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+	gsettings set org.gnome.desktop.interface icon-theme 'Yaru-purple-dark'
 #    notify-send "Night theme stablished"
 fi
